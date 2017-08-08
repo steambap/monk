1cabe376036d7f6616f4f837d9a67085f0085d14
Weilin Shi <934587911@qq.com>
2017-08-08 21:41:30 +0800

    parser let statement

--- parser/parser.go
+++ parser/parser.go
@@ -4,16 +4,19 @@ import (
 	"monk/ast"
 	"monk/lexer"
 	"monk/token"
+
+	"fmt"
 )
 
 type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors     []String
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []String{}}
 
 	// read 2 tokens
 	p.nextToken()
@@ -21,10 +24,75 @@ func New(l *lexer.Lexer) *Parser {
 
 	return p
 }
+
+func (p *Parser) Errors() {
+	return p.errors
+}
+func (p *Parser) peekErrors(t token.TokenType) {
+	msg := fmt.Sprintf("Expect next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+}
+
+func (p *Parser) parseStatement() *ast.Statement {
+	switch p.curToken.Type {
+		case token.LET:
+			return p.parseLetStatement()
+		default:
+			return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.Statement {
+	stmt := &ast.Statement{Token: p.curToken}
+
+	if !p.expectPeekToken(token.NAME) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeekToken(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO skip rvalue
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeekToken(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekErrors(t)
+		return false
+	}
 }
